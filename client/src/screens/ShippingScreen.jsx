547138c6c99dd1ea1132cd61a865844a5bf30032
Faisal786111 <faisalkhanisrar@gmail.com>
2024-04-17 19:15:24 +0530

    CheckoutSteps component implemented

--- client/src/screens/ShippingScreen.jsx
+++ client/src/screens/ShippingScreen.jsx
@@ -4,6 +4,7 @@ import { saveShippingAddress } from "../redux/slices/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
@@ -40,6 +41,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="address">
