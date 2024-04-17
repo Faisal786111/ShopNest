6ef810eafdf8fdc1403e3e1e3dbdc220dd294840
Faisal786111 <faisalkhanisrar@gmail.com>
2024-04-17 19:03:13 +0530

    private route component implemented

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,6 +14,7 @@ import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
 import ShippingScreen from "./screens/ShippingScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -24,13 +25,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <Route index element={<HomeScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart" element={<CartScreen />} />
-            <Route path="/login" element={<LoginScreen />}>
-              <Route path="shipping" element={<ShippingScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="" element={<PrivateRoute />}>
+              <Route path="/shipping" element={<ShippingScreen />} />
             </Route>
-            <Route path="/register" element={<RegisterScreen />}>
-              <Route path="shipping" element={<ShippingScreen />} />
-            </Route>
-            <Route path="/shipping" element={<ShippingScreen />} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
