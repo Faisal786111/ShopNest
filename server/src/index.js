8df39465e1282b111adb209c447ec80f9e8b37e4
Faisal786111 <faisalkhanisrar@gmail.com>
2024-04-27 22:34:21 +0530

    add image to uploads folder

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
 dotenv.config();
 require("./config/db");
 const cors = require("cors");
@@ -32,9 +34,16 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/uploads", uploadRoutes);
 app.use("/api/config/paypal", (req , res)=>{
     return res.json({clientId : process.env.PAYPAL_CLIENT_ID});
 });
+
+
+console.log("paht resolve" , path.resolve());
+const dirname = path.resolve();
+
+app.use("/uploads" , express.static(path.join(dirname , "/uploads")));
 
 //  Error middleware
 app.use(notFound);
---
+++ server/src/routes/uploadRoutes.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = new express.Router();
+const { auth, admin } = require("../middlewares/authMiddleware");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        // Extracting the file extension from the original filename
+        const ext = file.originalname.split('.').pop();
+        // Appending the unique suffix and file extension to the original filename
+        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + ext);
+    },
+});
+
+
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 1000000, //1MB
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error("Pleas upload only jpg , jpeg & png images"));
+        }
+
+        cb(undefined, true);
+    },
+});
+
+
+router.post("/", auth, upload.single("image"), async (req, res) => {
+    return res.status(201).json({
+        message: "Image uploaded successfully.",
+        file: req.file,
+    })
+}, (error, req, res, next) => {
+    res.status(400);
+    throw new Error(error.message);
+});
+
+router.get("/", auth,)
+
+
+module.exports = router;
